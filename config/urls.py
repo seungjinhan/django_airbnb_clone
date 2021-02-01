934b50e43422ebf8a051a8a914cc8a3314ef491c
seungjinhan <hanblues@gmail.com>
2021-02-01 23:52:16 +0900

    rooms page

--- config/urls.py
+++ config/urls.py
@@ -19,6 +19,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
+    path("", include("core.urls", namespace="core")),
     path("users/", include("users.urls", namespace="users")),
     path("admin/", admin.site.urls),
 ]
--- rooms/views.py
+++ rooms/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from . import models
 
-# Create your views here.
+
+def all_rooms(req):
+    all_rooms = models.Room.objects.all()
+    return render(req, "rooms/home.html", context={"rooms": all_rooms})
