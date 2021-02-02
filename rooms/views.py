cf5ec2340341fc5180e2604cec6dff1fa3047b4d
seungjinhan <hanblues@gmail.com>
2021-02-02 23:32:05 +0900

    main page

--- rooms/views.py
+++ rooms/views.py
@@ -1,7 +1,16 @@
+from math import ceil
 from django.shortcuts import render
+from django.core.paginator import Paginator
 from . import models
 
 
 def all_rooms(req):
-    all_rooms = models.Room.objects.all()
-    return render(req, "rooms/home.html", context={"rooms": all_rooms})
+    page = req.GET.get("page", 1)
+    room_list = models.Room.objects.all()
+    paginator = Paginator(room_list, 10)
+    rooms = paginator.get_page(page)
+    return render(
+        req,
+        "rooms/home.html",
+        {"rooms":rooms},
+    )
