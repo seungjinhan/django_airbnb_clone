681f48cb09124540b0084074a9f6c2bc79158e4a
seungjinhan <hanblues@gmail.com>
2021-02-03 23:39:26 +0900

    room class

--- rooms/views.py
+++ rooms/views.py
@@ -1,20 +1,33 @@
-from django.shortcuts import render, redirect
-from django.core.paginator import Paginator, EmptyPage
+from django.utils import timezone
+from django.views.generic import ListView
+# from django.shortcuts import render, redirect
+# from django.core.paginator import Paginator, EmptyPage
 from . import models
 
 
-def all_rooms(req):
-    page = req.GET.get("page", 1)
-    room_list = models.Room.objects.all()
-    paginator = Paginator(room_list, 10, orphans=5)
-    try:
-        rooms = paginator.page(int(page))
-        return render(
-            req,
-            "rooms/home.html",
-            {"page": rooms},
-        )
-    except EmptyPage:
-        #rooms = paginator.page(1)
-        return redirect("/")
-    # rooms = paginator.get_page(page)
+class HomeView(ListView):
+
+    """ Home View """
+    
+    model = models.Room
+    paginate_by = 10
+    ordering = 'created'
+    paginate_orphans = 5
+    context_object_name = 'rooms'
+
+
+# def all_rooms(req):
+#     page = req.GET.get("page", 1)
+#     room_list = models.Room.objects.all()
+#     paginator = Paginator(room_list, 10, orphans=5)
+#     try:
+#         rooms = paginator.page(int(page))
+#         return render(
+#             req,
+#             "rooms/home.html",
+#             {"page": rooms},
+#         )
+#     except EmptyPage:
+#         #rooms = paginator.page(1)
+#         return redirect("/")
+#     # rooms = paginator.get_page(page)
