693127f0bd18fab4d0b4a53b9eea7e1f2e43c70b
seungjinhan <hanblues@gmail.com>
2021-02-07 17:23:33 +0900

    room url

--- rooms/views.py
+++ rooms/views.py
@@ -1,4 +1,5 @@
 from django.utils import timezone
+from django.shortcuts import render
 from django.views.generic import ListView
 # from django.shortcuts import render, redirect
 # from django.core.paginator import Paginator, EmptyPage
@@ -8,13 +9,16 @@ from . import models
 class HomeView(ListView):
 
     """ Home View """
-    
+   
     model = models.Room
     paginate_by = 10
     ordering = 'created'
     paginate_orphans = 5
     context_object_name = 'rooms'
 
+
+def room_detail(request, pk):
+    return render(request, "rooms/detail.html")
 
 # def all_rooms(req):
 #     page = req.GET.get("page", 1)
